653c1d035d1438abdee4aaa614c307440ae6a485
jaehee329 <jaehee329@yonsei.ac.kr>
2022-10-21 15:48:07 +0900

    bfs fixed wrong answer

--- Baekjoon/Graph/a1707.java
+++ Baekjoon/Graph/a1707.java
@@ -1,61 +1,69 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Iterator;
+import java.io.*;
+import java.util.*;
 
 public class a1707 {
-    static int N;
-    static boolean[] visit;
-    static List<LinkedList<Integer>> graph;
-    static boolean[] color;
-    static boolean flag;
-    public static void main(String[] args) throws IOException{
+    private static int numOfVertex;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int g = Integer.parseInt(st.nextToken());
-        for(int k=1; k<=g; k++){
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken()); int M = Integer.parseInt(st.nextToken());
-            graph = new ArrayList<LinkedList<Integer>>();
-            for(int i=0; i<=N; i++){
-                graph.add(new LinkedList<Integer>());
-            }
-            visit = new boolean[N+1];
-            color = new boolean[N+1];
-            Arrays.fill(visit, true);
-            for(int i=1; i<=M; i++){
-                st = new StringTokenizer(br.readLine());
-                int start = Integer.parseInt(st.nextToken()); int end = Integer.parseInt(st.nextToken());
-                graph.get(start).add(end); graph.get(end).add(start);
-            }
-            flag = false;
-            dfs(1, true);
-            for(int i=1; i<=N; i++){
-                if(visit[i]){
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag) System.out.println("NO");
-            else System.out.println("YES");
+        int K = Integer.parseInt(br.readLine());
+        for (int i=0; i<K; i++) {
+            printBipartity(br);
         }
     }
-	public static void dfs(int start, boolean c) {
-		visit[start] = false;
-        color[start] = c;
-        Iterator<Integer> iter = graph.get(start).iterator();
-        while(iter.hasNext()){
-            int target = iter.next();
-            if(visit[target]){
-                dfs(target, !c);
-            } else if(color[target]==c){
-                flag = true;
+    
+    private static void printBipartity(BufferedReader br) throws IOException {
+        ArrayList<ArrayList<Integer>> graph = getGraph(br);
+        boolean biparitite = checkBiparity(graph);
+        if (biparitite) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    private static ArrayList<ArrayList<Integer>> getGraph(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        numOfVertex = Integer.parseInt(st.nextToken());
+        int numOfEdge = Integer.parseInt(st.nextToken());
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i=0; i<numOfVertex+1; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i=0; i<numOfEdge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            graph.get(v1).add(v2);
+            graph.get(v2).add(v1);
+        }
+        return graph;
+    }
+
+    private static boolean checkBiparity(ArrayList<ArrayList<Integer>> graph) {
+        int[] visit = new int[numOfVertex+1];
+        int group = 1;
+        for (int i=1; i<=numOfVertex; i++) {
+            if (visit[i]==0) {
+                Queue<Integer> q = new LinkedList<>();
+                visit[i] = group;
+                q.add(i);
+                while (!q.isEmpty()) {
+                    int src = q.poll();
+                    int srcGroup = visit[src];
+                    for (int j=0; j<graph.get(src).size(); j++) {
+                        int dst = graph.get(src).get(j);
+                        if (visit[dst]==0) {
+                            visit[dst] = -1*srcGroup;
+                            q.add(dst);
+                        } else if (visit[dst] != -1*srcGroup) {
+                             return false;
+                        }
+                    }
+                }
             }
         }
-	}
+        return true;
+    }
 }
