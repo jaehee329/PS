327e03ad579b011f1684d9b5e5e35ae9efd301f0
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-14 20:20:23 +0900

    BOJ 2108: counting sort

--- Baekjoon/stream.java
+++ Baekjoon/stream.java
@@ -1,12 +1,12 @@
 import java.util.*;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class stream {
     public static void main(String[] args) {
-        List<Integer> idealList = List.of(1,2,3);
-        List<Integer> answerList = List.of(2, 3, 1);
-        IntStream intStream = answerList.stream().mapToInt(Integer::intValue);
-        int ball = (int)intStream.filter((s)->idealList.contains(s)).count();
-        System.out.println(ball);
+        String s = "323";
+        IntStream intStream = s.chars();
+        int cnt = (int)intStream.distinct().count();
+        System.out.println(cnt);
     }
 }
