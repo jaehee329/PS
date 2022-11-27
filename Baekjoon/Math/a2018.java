7f52b286467c1028397fd4e77e68845cead16e2a
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-27 21:30:52 +0900

    BOJ 1764: Set, BOJ 2407: BigInteger

--- Baekjoon/Math/a2018.java
+++ Baekjoon/Math/a2018.java
@@ -1,5 +1,5 @@
 import java.io.*;
-public class Main{
+public class a2018 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
