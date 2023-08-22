958a785e12802595623040f7760985f388b05664
jaehee329 <jjhs9803@gmail.com>
2023-08-22 16:28:59 +0900

    BOJ 2294: 동전 2, DP

--- Baekjoon/DP/a2294.java
+++ Baekjoon/DP/a2294.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class a2293 {
+public class a2294 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,13 +10,19 @@ public class a2293 {
         int sum = Integer.parseInt(st.nextToken());
         int[] values = new int[types + 1];
         int[] dp = new int[sum + 1];
-        dp[0] = 1;
+        Arrays.fill(dp, 100_001);
+        dp[0] = 0;
         
         for(int i = 1; i <= types; i++) {
             values[i] = Integer.parseInt(br.readLine());
             for (int j = values[i]; j <= sum; j++) {
-                dp[j] += dp[j - values[i]];
+                dp[j] = Math.min(dp[j], dp[j - values[i]] + 1);
             }
+        }
+        
+        if (dp[sum] == 100_001) {
+            System.out.println(-1);
+            return;
         }
         System.out.println(dp[sum]);
     }
