ebe4df6f94b48401c5f9d3defa4dde0263ceb47f
jaehee329 <jjhs9803@gmail.com>
2023-12-04 12:12:36 +0900

    BOJ 23968: 알고리즘 수업 - 버블 정렬 1, Bubble Sort

---
+++ Baekjoon/Sort/a23968.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class a23968 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        bubbleSort(arr, k);
+    }
+
+    private static void bubbleSort(int[] arr, int k) {
+        int cnt = 0;
+        for (int last = arr.length - 1; last > 0; last--) {
+            for (int i = 0; i < last; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    cnt++;
+                    if (cnt == k) {
+                        System.out.println(Math.min(arr[i], arr[i + 1]) + " " + Math.max(arr[i], arr[i + 1]));
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(-1);
+    }
+}
